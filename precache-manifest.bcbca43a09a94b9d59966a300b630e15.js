d7b7a89c80f33b67fa45d7e8bd49574f862f75b5
Sumit Rawat <33266129+SUMIT30AUGUST@users.noreply.github.com>
2022-10-12 00:42:55 +0530

    Updates

---
+++ precache-manifest.bcbca43a09a94b9d59966a300b630e15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfdc02e5d81f58ba86e0",
+    "url": "/TextUtils-Reactjs/static/css/main.015e2488.chunk.css"
+  },
+  {
+    "revision": "cfdc02e5d81f58ba86e0",
+    "url": "/TextUtils-Reactjs/static/js/main.cfdc02e5.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-Reactjs/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-Reactjs/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "f1d6cae0324dc6d6de3a",
+    "url": "/TextUtils-Reactjs/static/js/runtime~main.f1d6cae0.js"
+  },
+  {
+    "revision": "541f2e6cf3b853d6ddc58483af2d71a0",
+    "url": "/TextUtils-Reactjs/index.html"
+  }
+];
